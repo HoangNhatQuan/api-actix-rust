04a1e4fb83b5181575765884cc4f14fc22a813f4
quanhoang882002 <135295387+hnquan8@users.noreply.github.com>
2024-03-01 08:46:16 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn index(number: web::Json<Number>) -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/calSum", web::post().to(index)))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }

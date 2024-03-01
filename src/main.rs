51ddaee4482727fd77f776bf3010a029d558867c
quanhoang882002 <135295387+hnquan8@users.noreply.github.com>
2024-03-01 11:13:23 +0700

    add generic types

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,22 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-struct Number {
-    a: i32,
-    b: i32,
+struct Number<T> {
+    a: T,
+    b: T,
 }
 
-async fn index(number: web::Json<Number>) -> impl Responder {
+async fn index<T: std::ops::Add<Output = T> + Copy + std::fmt::Display>(
+    number: web::Json<Number<T>>,
+) -> impl Responder {
     let sum = number.a + number.b;
     HttpResponse::Ok().body(format!("Sum: {}", sum))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/calSum", web::post().to(index)))
-        .bind(("0.0.0.0", 8080))?
+    HttpServer::new(|| App::new().route("/calSum", web::post().to(index::<i32>)))
+        .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
